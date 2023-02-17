82416271d0d17222d7bddc71477e35f0f292586b
pabloTokuo <contatopbo@hotmail.com>
2023-02-17 15:07:18 -0300

    JavaScript tipos de dados

---
+++ JavaScript/tipos-de-dados/script.js
@@ -0,0 +1,18 @@
+// Declare uma variável contendo uma string
+var string = "String";
+// Declare uma variável contendo um número dentro de uma string
+var numero = "25";
+// Declare uma variável com a sua idade
+var idade = 25;
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var nome, sobrenome;
+nome = "Pablo";
+sobrenome = "Yuri";
+
+var nomeCompleto = `${nome} ${sobrenome}`;
+// Coloque a seguinte frase em uma variável: It's time
+var variavel = "It's time"
+// Verifique o tipo da variável que contém o seu nome
+var verificarNome = typeof nome;
+console.log(verificarNome);
