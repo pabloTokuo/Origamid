1cd8cc921e3ab15ccd9e917a62d1571eb6399814
pabloTokuo <contatopbo@hotmail.com>
2023-02-17 15:20:33 -0300

    js numeros e operadores

---
+++ JavaScript/numeros-e-operadores/script.js
@@ -0,0 +1,18 @@
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20;
+
+// Crie duas expressões que retornem NaN
+var expressao1 = total / "teste";
+var expressao2 = total - "teste";
+// Somar a string '200' com o número 50 e retornar 250
+var soma = +"200" + 50;
+
+// Incremente o número 5 e retorne o seu valor incrementado
+var incremente = 5;
+console.log(++incremente);
+
+// Como dividir o peso por 2?
+var numero = +'80' / 2;
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+
