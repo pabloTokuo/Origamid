a04b897a04bfb0bce77b4e0ac83494d706c982c1
pabloTokuo <contatopbo@hotmail.com>
2023-02-17 14:43:11 -0300

    JavaScript Variaveis

---
+++ JavaScript/variaveis/script.js
@@ -0,0 +1,15 @@
+// Declare uma variável contendo uma string
+var nome = "Pablo";
+
+// Declare uma variável com a sua idade
+var idade = 25;
+
+// Declarar uma variavel com a sua comida
+// favorita e nao atribuir valor
+var comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = "Macarrao";
+
+// Declarar 5 variaveis diferentes sem valores
+var um, dois, tres, quatro, cinco;
