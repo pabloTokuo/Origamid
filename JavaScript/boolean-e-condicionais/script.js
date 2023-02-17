2d96c29ef9b844736b266d147f69c421af2dec8d
pabloTokuo <contatopbo@hotmail.com>
2023-02-17 15:33:44 -0300

    js boolean e condicionais

---
+++ JavaScript/boolean-e-condicionais/script.js
@@ -0,0 +1,49 @@
+// Verifique se a sua idade é maior do que a de algum parente
+// Dependendo do resultado coloque no console 'É maior', 'É igual' ou 'É menor'
+
+var minhaIdade = 25;
+var idadeParente = 25;
+
+if (minhaIdade > idadeParente) {
+  console.log("maior");
+} else if (minhaIdade == idadeParente) {
+  console.log("igual");
+} else {
+  console.log("menor");
+}
+
+// Qual valor é retornado na seguinte expressão?
+var expressao = (5 - 2) && (5 - ' ') && (5 - 2);
+
+// Verifique se as seguintes variáveis são Truthy ou Falsy
+var nome = 'Andre';
+var idade = 28;
+var possuiDoutorado = false;
+var empregoFuturo;
+var dinheiroNaConta = 0;
+
+console.log(!!nome, !!idade, !!possuiDoutorado, !! empregoFuturo, !!dinheiroNaConta);
+
+// Compare o total de habitantes do Brasil com China (valor em milhões)
+var brasil = 207;
+var china = 1340;
+
+if(brasil > china) {
+  console.log("Brasil mais habitantes");
+} else {
+  console.log("China mais habitantes");
+}
+
+// O que irá aparecer no console?
+if(('Gato' === 'gato') && (5 > 2)) {
+  console.log('Verdadeiro');
+} else {
+  console.log('Falso');
+}
+
+// O que irá aparecer no console?
+if(('Gato' === 'gato') || (5 > 2)) {
+  console.log('Gato' && 'Cão');
+} else {
+  console.log('Falso');
+}
