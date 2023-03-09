2c6b65043ea1747e66fb7fb6f620e9ee2f44c767
pabloTokuo <contatopbo@hotmail.com>
2023-03-09 13:49:57 -0300

    Number e Math

---
+++ number-e-math/script.js
@@ -0,0 +1,30 @@
+// Retorne um número aleatório
+// entre 1050 e 2000
+console.log(Math.floor(Math.random() * (1050 - 2000 + 1) + 2000)); 
+
+// Retorne o maior número da lista abaixo
+const numeros = '4, 5, 20, 8, 9';
+
+const arrayNumeros = numeros.split(", ");
+const numeroMaximo = Math.max(...arrayNumeros);
+
+// Crie uma função para limpar os preços
+// e retornar os números com centavos arredondados
+// depois retorne a soma total
+const listaPrecos = ['R$ 59,99', ' R$ 100,222',
+                     'R$ 230  ', 'r$  200'];
+
+function limparPreco(preco) {
+  preco = +preco.toUpperCase().replace("R$ ", "").trim().replace(",", ".");
+  preco = +preco.toFixed(2);
+  return preco;
+}      
+
+let soma = 0;
+listaPrecos.forEach((preco) => {
+  soma += limparPreco(preco);
+})
+
+console.log(soma.toLocaleString("pt-BR", {style: "currency", currency: "BRL"}));
+
+limparPreco(listaPrecos[1]);
