3e29ff204e46333e8431f5f50cee11fc025c5948
pabloTokuo <contatopbo@hotmail.com>
2023-04-14 11:16:51 -0300

    Data Object

---
+++ date-objects/js/modules/tooltip.js
@@ -0,0 +1,43 @@
+export default function initToolTip() {
+  const tooltips = document.querySelectorAll("[datatooltip]");
+
+  tooltips.forEach((item) => {
+    item.addEventListener("mouseover", onMouseOver);
+  })
+
+  function onMouseOver(event) {
+    const tooltipBox = criarToolTipBox(this);
+    tooltipBox.style.top = event.pageY + 'px';
+    tooltipBox.style.left = event.pageX + 'px';
+
+    onMouseMove.tooltipBox = tooltipBox;
+    this.addEventListener('mousemove', onMouseMove);
+
+    onMouseLeave.tooltipBox =  tooltipBox;
+    onMouseLeave.element = this;
+    this.addEventListener('mouseleave', onMouseLeave);
+  }
+
+  const onMouseLeave = {
+    handleEvent() {
+      this.tooltipBox.remove();
+      this.element.removeEventListener('mouseleave', onMouseLeave);
+    }
+  }
+
+  const onMouseMove = {
+    handleEvent(event) {
+      this.tooltipBox.style.top = event.pageY + 20 + 'px';
+      this.tooltipBox.style.left = event.pageX + 20 + 'px';
+    }
+  }
+
+  function criarToolTipBox(element) {
+    const tooltipBox = document.createElement("div");
+    const text = element.getAttribute("aria-label");
+    tooltipBox.classList.add("tooltip");
+    tooltipBox.innerText = text;
+    document.body.appendChild(tooltipBox);
+    return tooltipBox;
+  }
+}
---
+++ date-objects/js/script.js
@@ -0,0 +1,21 @@
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimacaoScroll from "./modules/scroll-animacao.js";
+import initAccordion from "./modules/accordion.js"; 
+import initTabNav from "./modules/tabnav.js";
+import initModal from "./modules/modal.js";
+import initToolTip from "./modules/tooltip.js";
+import initDropDown from "./modules/dropdown-menu.js";
+import initMenuMobile from "./modules/menu-mobile.js";
+import initAnimaNumeros from "./modules/anima-numeros.js";
+import initFuncionamento from "./modules/funcionamento.js";
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
+initModal();
+initToolTip();
+initDropDown();
+initMenuMobile();
+initAnimaNumeros();
+initFuncionamento();
