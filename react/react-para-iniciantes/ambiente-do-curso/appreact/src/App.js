ff7785812d0aec02c2c04375031c5b3776dca441
pabloTokuo <contatopbo@hotmail.com>
2023-05-02 10:00:32 -0300

    Eventos

--- react/react-para-iniciantes/ambiente-do-curso/appreact/src/App.js
+++ react/react-para-iniciantes/ambiente-do-curso/appreact/src/App.js
@@ -1,48 +1,13 @@
-// Organize os produtos como mostrado no vídeo
-// Mostre apenas produtos que forem mais caros que R$ 1500
-
-const produtos = [
-  {
-    id: 1,
-    nome: 'Smartphone',
-    preco: 'R$ 2000',
-    cores: ['#29d8d5', '#252a34', '#fc3766'],
-  },
-  {
-    id: 2,
-    nome: 'Notebook',
-    preco: 'R$ 3000',
-    cores: ['#ffd045', '#d4394b', '#f37c59'],
-  },
-  {
-    id: 3,
-    nome: 'Tablet',
-    preco: 'R$ 1500',
-    cores: ['#365069', '#47c1c8', '#f95786'],
-  },
-];
-
 const App = () => {
-  const dados = produtos.filter(
-    ({ preco }) => Number(preco.replace('R$ ', '')) > 1500,
-  );
+  function handleScroll(e) {
+    console.log(e);
+  }
+  window.addEventListener('scroll', handleScroll);
 
   return (
-    <section>
-      {dados.map(({ id, nome, preco, cores }) => (
-        <div key={id}>
-          <h1 key={nome}>{nome}</h1>
-          <p key={preco}>Preco: {preco}</p>
-          <ul>
-            {cores.map((cor) => (
-              <li key={cor} style={{ backgroundColor: cor, color: 'white' }}>
-                {cor}
-              </li>
-            ))}
-          </ul>
-        </div>
-      ))}
-    </section>
+    <div style={{ height: '200vh' }}>
+      <button onClick={(e) => alert(e.target.innerText)}>Clique</button>
+    </div>
   );
 };
 
