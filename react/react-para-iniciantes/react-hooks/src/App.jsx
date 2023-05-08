85852879a58dfb16fc1594853645c5dd268325d1
pabloTokuo <contatopbo@hotmail.com>
2023-05-08 11:56:32 -0300

    custom hooks

--- react/react-para-iniciantes/react-hooks/src/App.jsx
+++ react/react-para-iniciantes/react-hooks/src/App.jsx
@@ -1,13 +1,41 @@
 import React from 'react';
-import Produto from './Produto';
-import { GlobalStorage } from './GlobalStorage';
+import useLocalStorage from './useLocalStorage';
+import useFetch from './useFetch';
 
 const App = () => {
-  return (
-    <GlobalStorage>
-      <Produto />
-    </GlobalStorage>
-  );
+  const [produto, setProduto] = useLocalStorage('produto', '');
+  const { request, data, loading, error } = useFetch();
+
+  React.useEffect(() => {
+    async function fetchData() {
+      const { response, json } = await request(
+        'https://ranekapi.origamid.dev/json/api/produto/',
+      );
+    }
+    fetchData();
+  }, [request]);
+
+  if (error) return <p>{error}</p>;
+  if (loading === true) return <p>Carregando...</p>;
+  if (data)
+    return (
+      <div>
+        <p>Produto preferido: {produto}</p>
+        <button onClick={({ target }) => setProduto(target.innerText)}>
+          notebook
+        </button>
+        <button onClick={({ target }) => setProduto(target.innerText)}>
+          smartphone
+        </button>
+
+        {data.map((produto, index) => (
+          <div key={index}>
+            <h1>{produto.nome}</h1>
+          </div>
+        ))}
+      </div>
+    );
+  else return null;
 };
 
 export default App;
