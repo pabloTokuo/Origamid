92f7b74bb56a4eb3c2103fbc5cd9d5a3ff25ee7a
pabloTokuo <contatopbo@hotmail.com>
2023-05-09 15:29:15 -0300

    React Checkbox

--- react/react-para-iniciantes/react-forms/src/App.jsx
+++ react/react-para-iniciantes/react-forms/src/App.jsx
@@ -1,25 +1,35 @@
 import React from 'react';
 
+const coresArray = ['azul', 'roxo', 'laranja', 'verde', 'vermelho', 'cinza'];
+
 function App() {
-  const [radio, setRadio] = React.useState('');
+  const [cores, setCores] = React.useState([]);
+
+  function handleChange({ target }) {
+    if (target.checked) {
+      setCores([...cores, target.value]);
+    } else {
+      setCores(cores.filter((cor) => cor !== target.value));
+    }
+  }
+
+  function handleChecked(cor) {
+    return cores.includes(cor);
+  }
 
   return (
-    <form action="">
-      <label>Notebook</label>
-      <input
-        type="radio"
-        value="notebook"
-        name="produto"
-        onChange={({ target }) => setRadio(target.value)}
-      />
-      <label>Smartphone</label>
-      <input
-        type="radio"
-        value="smartphone"
-        name="produto"
-        onChange={({ target }) => setRadio(target.value)}
-      />
-      {radio}
+    <form>
+      {coresArray.map((cor, index) => (
+        <label key={index} style={{ textTransform: 'capitalize' }}>
+          <input
+            type="checkbox"
+            value={cor}
+            checked={handleChecked(cor)}
+            onChange={handleChange}
+          />
+          {cor}
+        </label>
+      ))}
     </form>
   );
 }
