fc77aa29bf26589128084d1be6415de56be4d9c9
pabloTokuo <contatopbo@hotmail.com>
2023-05-12 12:15:43 -0300

    Head

--- react/react-para-iniciantes/react-router/src/Home.jsx
+++ react/react-para-iniciantes/react-router/src/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Head from './Head';
 
 const Home = () => {
   return (
     <div>
+      <Head title="Pagina Home" description="Descricao da Home" />
       <h1>Home</h1>
       <p>Esse 'e a home'</p>
       <Link to={'produto/notebook'}>Notebook</Link> |
