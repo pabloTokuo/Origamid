f6f8317c0a0c7de9b92c166c9f668a9df214a0c3
pabloTokuo <contatopbo@hotmail.com>
2023-05-16 14:33:19 -0300

    Login Routes

---
+++ react/dog/src/Components/Login/LoginForm.jsx
@@ -0,0 +1,51 @@
+import { Link } from 'react-router-dom';
+import React from 'react';
+
+const LoginForm = () => {
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    })
+      .then((r) => {
+        console.log(r);
+        return r.json();
+      })
+      .then((json) => {
+        console.log(json);
+        return json;
+      });
+  }
+
+  return (
+    <section>
+      <h1>Login</h1>
+      <form action="" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={username}
+          onChange={({ target }) => setUsername(target.value)}
+        />
+        <input
+          type="text"
+          value={password}
+          onChange={({ target }) => setPassword(target.value)}
+        />
+        <button>Entrar</button>
+      </form>
+      <Link to="/login/criar">Cadastro</Link>
+    </section>
+  );
+};
+
+export default LoginForm;
--- react/dog/vite.config.js
+++ react/dog/vite.config.js
@@ -5,12 +5,5 @@ import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    eslintPlugin({
-      cache: false,
-      include: ['./src/**/*.js', './src/**/*.jsx'],
-    }),
-    svgr(),
-  ],
+  plugins: [react(), svgr()],
 });
