5dc18ab4f4a242be446ed59709ce3817ed38ce0d
pabloTokuo <contatopbo@hotmail.com>
2023-05-17 16:02:19 -0300

    Protected Route

--- react/dog/src/App.jsx
+++ react/dog/src/App.jsx
@@ -5,6 +5,8 @@ import Header from './Components/Header';
 import Home from './Components/Home';
 import Login from './Components/Login/Login';
 import { UserStorage } from './UserContext';
+import ProtectedRoute from './Components/Helper/ProtectedRoute';
+import User from './Components/User/User';
 
 const App = () => {
   return (
@@ -14,7 +16,15 @@ const App = () => {
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login/*" element={<Login />} />
+            <Route path="login/*" element={<Login />} />
+            <Route
+              path="conta/*"
+              element={
+                <ProtectedRoute>
+                  <User />
+                </ProtectedRoute>
+              }
+            />
           </Routes>
           <Footer />
         </UserStorage>
