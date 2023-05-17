0eb2144b9a3a2feddf1568378dc61c825112bee8
pabloTokuo <contatopbo@hotmail.com>
2023-05-17 15:16:30 -0300

    useFetch

--- react/dog/src/Hooks/useForm.jsx
+++ react/dog/src/Hooks/useForm.jsx
@@ -6,6 +6,10 @@ const types = {
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     message: 'Preencha um email valido',
   },
+  password: {
+    regex: /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/,
+    message: 'A senha deve conter fraca.',
+  },
 };
 
 const useForm = (type) => {
