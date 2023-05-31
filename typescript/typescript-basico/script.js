f3b56a1e24371c671b86e7612402ae871b349ad9
pabloTokuo <contatopbo@hotmail.com>
2023-05-31 12:03:34 -0300

    Types e Interfaces

--- typescript/typescript-basico/script.js
+++ typescript/typescript-basico/script.js
@@ -1,14 +1,18 @@
 "use strict";
-function toNumber(numero) {
-    if (typeof numero === 'number') {
-        return 'number';
-    }
-    else if (typeof numero === 'string') {
-        return 'string';
-    }
-    else {
-        throw new Error("Valor deve ser um numero ou string");
-    }
+async function fetchProduct() {
+    const response = await fetch('https://api.origamid.dev/json/notebook.json');
+    const data = await response.json();
+    showProduct(data);
 }
-const valor = toNumber('#');
-console.log(valor);
+fetchProduct();
+function showProduct(data) {
+    document.body.innerHTML = `
+    <div>
+      <h2>${data.nome}</h2>
+      <p>R$ ${data.preco}</p>
+      <p>${data.descricao}</p>
+      <h3>${data.empresaFabricante.nome}</h3>
+      <h3>${data.empresaMontadora.nome}</h3>
+    </div>
+  `;
+}
--- typescript/typescript-basico/script.ts
+++ typescript/typescript-basico/script.ts
@@ -1,12 +1,33 @@
-function toNumber(numero:number | string) {
-  if(typeof numero === 'number') {
-    return 'number';
-  } else if (typeof numero === 'string') {
-    return 'string';
-  } else {
-    throw new Error("Valor deve ser um numero ou string");
-  }
+interface Empresa {
+  nome: string;
+  fundacao: number;
+  pais: string;
 }
 
-const valor = toNumber('#');
-console.log(valor);
+interface Produto {
+  nome: string;
+  preco: number;
+  descricao: string;
+  empresaFabricante: Empresa;
+  empresaMontadora: Empresa;
+}
+
+async function fetchProduct() {
+  const response = await fetch('https://api.origamid.dev/json/notebook.json');
+  const data = await response.json();
+  showProduct(data);
+}
+
+fetchProduct();
+
+function showProduct(data: Produto) {
+  document.body.innerHTML = `
+    <div>
+      <h2>${data.nome}</h2>
+      <p>R$ ${data.preco}</p>
+      <p>${data.descricao}</p>
+      <h3>${data.empresaFabricante.nome}</h3>
+      <h3>${data.empresaMontadora.nome}</h3>
+    </div>
+  `;
+}
