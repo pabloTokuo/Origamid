0aae789de2b9c49edcc79624ea6ea6e4f6650d34
pabloTokuo <contatopbo@hotmail.com>
2023-05-31 11:28:36 -0300

    Union types exercicios

--- typescript/typescript-basico/script.js
+++ typescript/typescript-basico/script.js
@@ -1,23 +1,14 @@
 "use strict";
-function normalizarTexto(texto) {
-    return texto.trim().toLowerCase();
-}
-const input = document.querySelector('input');
-const total = localStorage.getItem('total');
-if (input && total) {
-    input.value = total;
-    calcularGanho(Number(input.value));
-}
-function calcularGanho(value) {
-    const p = document.querySelector('p');
-    if (p)
-        p.innerText = `ganho total: ${value + 100 - value * 0.2}`;
-}
-function totalMudou() {
-    if (input) {
-        localStorage.setItem('total', input.value);
-        calcularGanho(Number(input.value));
+function toNumber(numero) {
+    if (typeof numero === 'number') {
+        return 'number';
+    }
+    else if (typeof numero === 'string') {
+        return 'string';
+    }
+    else {
+        throw new Error("Valor deve ser um numero ou string");
     }
 }
-if (input)
-    input.addEventListener('keyup', totalMudou);
+const valor = toNumber('#');
+console.log(valor);
--- typescript/typescript-basico/script.ts
+++ typescript/typescript-basico/script.ts
@@ -1,29 +1,12 @@
-function normalizarTexto(texto: string) {
-  return texto.trim().toLowerCase();
-}
-
-const input = document.querySelector('input');
-
-const total = localStorage.getItem('total');
-if(input && total) {
-  input.value = total;
-  calcularGanho(Number(input.value));
-}
-
-
-function calcularGanho(value: number) {
-  const p = document.querySelector('p');
-  if(p)
-  p.innerText = `ganho total: ${value + 100 - value * 0.2}`;
-}
-
-function totalMudou() {
-  if(input) {
-    localStorage.setItem('total', input.value);
-    calcularGanho(Number(input.value));
+function toNumber(numero:number | string) {
+  if(typeof numero === 'number') {
+    return 'number';
+  } else if (typeof numero === 'string') {
+    return 'string';
+  } else {
+    throw new Error("Valor deve ser um numero ou string");
   }
-
 }
 
-if(input)
-input.addEventListener('keyup', totalMudou);
+const valor = toNumber('#');
+console.log(valor);
